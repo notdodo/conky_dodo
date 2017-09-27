58a4d1dab968f01be5d03b81e664d23989ff267f
Ahmed Samir <ahmed.samer@gmail.com>
2017-09-27 12:31:04 +0200

    fix scrip path

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 killall conky
-conky -d -c "$HOME/.conky/Dodo/conky_dodoCPU" &
-conky -d -c "$HOME/.conky/Dodo/conky_dodoDATA" &
+conky -d -c "$HOME/.conky/conky_dodo/conky_dodoCPU" &
+conky -d -c "$HOME/.conky/conky_dodo/conky_dodoDATA" &
 
 # WIP
-conky -d -c "$HOME/.conky/Dodo/conky_dodoNET" &
+conky -d -c "$HOME/.conky/conky_dodo/conky_dodoNET" &
